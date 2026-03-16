60b121064e31b9cab57d57bb21ac5851e06c6cec
Alexander Holten Udengaard <alhu@Mac-Studio-DAB-Lejerbo.local>
2026-03-16 15:20:30 +0100

    Rekv

---
+++ niels_værdier/indsæt_regnskabstekster.sql
@@ -0,0 +1,44 @@
+SELECT
+    x.sel,
+    x.afd,
+    x.SAtype,
+    x.Nr,
+    x.Teksttype,
+    x.Navn,
+    case
+        when rn.value = 1 then 1
+        else 0
+    end as Værdi
+FROM
+    (
+        SELECT
+            a.sel,
+            a.afd,
+            a.navn as afdnavn,
+            def.*,
+            0 as value
+        FROM
+            [Bolig].[dbo].[Afdeling] a
+            CROSS APPLY (
+                SELECT
+                    *
+                FROM
+                    [Bolig].[dbo].[Regnskabsteksttyper] rt
+                    LEFT JOIN (
+                        select
+                            distinct tekst
+                        from
+                            [LejerBO_DB].[dbo].[Regnskabstekster_Niels]
+                    ) rn on rt.navn = rn.tekst
+                where
+                    rn.tekst is not null
+            ) def
+    ) x
+    LEFT JOIN [LejerBO_DB].[dbo].[Regnskabstekster_Niels] rn ON x.sel = rn.sel
+    and x.afd = rn.afd
+    and x.navn = rn.tekst
+where
+    x.sel > 100
+order by
+    x.sel,
+    x.afd
